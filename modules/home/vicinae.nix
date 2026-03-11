f8df15cc9bec8f33a336a55cea72f889bc8b197e
Patin Muangjan <patin@patin.dev>
2026-03-12 06:51:41 +0700

    update vicinae config

--- modules/home/vicinae.nix
+++ modules/home/vicinae.nix
@@ -1,12 +1,22 @@
+{ pkgs, lib, ... }:
+
 {
   services.vicinae = {
     enable = true;
+    package = pkgs.vicinae;
     systemd = {
       enable = true;
       autoStart = true;
       environment = {
         USE_LAYER_SHELL = 1;
       };
+    };
+    settings = {
+      "$schema" = "https://vicinae.com/schemas/config.json";
+      launcher_window = lib.mkForce {
+        opacity = 0.80;
+      };
+      pop_to_root_on_close = true;
     };
   };
 }
