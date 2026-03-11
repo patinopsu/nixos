fcfc30aee72a33ceaefb1486c28c8506753e4ddf
Patin Muangjan <patin@patin.dev>
2026-03-11 22:35:30 +0700

    remove portalPackage and Dolphin and enable udisks2

--- modules/gui.nix
+++ modules/gui.nix
@@ -1,6 +1,7 @@
 {inputs, pkgs, lib, ...}:
 
 {
+  services.udisks2.enable = true;
   services.displayManager.ly = {
     enable = true;
     settings = {
@@ -17,16 +18,7 @@
     enable = true;
     withUWSM  = true;
     package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
-    portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
   };
-  environment.systemPackages = with pkgs.kdePackages; [
-    kio
-    kio-fuse
-    kio-extras
-    qtsvg
-    dolphin
-  ];
-  services.udisks2.enable = true;
   services.gnome.gnome-keyring.enable = true;
   xdg = {
     portal = {
