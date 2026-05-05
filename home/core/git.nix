6d1dc385308451153b953fce064ea6e45daf8366
Patin Muangjan <patin@patin.dev>
2026-05-05 10:08:25 +0700

    Set Username and Email

--- home/core/git.nix
+++ home/core/git.nix
@@ -3,6 +3,10 @@
     programs.git = {
       enable = true;
       settings = {
+        user = {
+          name = "Patin Muangjan";
+          email = "patin@patin.dev";
+        };
         safe = {
           directory = "/etc/cfg/";
         };
