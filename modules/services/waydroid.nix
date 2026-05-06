af72c5064e1db58231b92e50e9e741faf80df054
Patin Muangjan <patin@patin.dev>
2026-05-07 06:55:02 +0700

    fix waydroid

--- modules/services/waydroid.nix
+++ modules/services/waydroid.nix
@@ -1,6 +1,10 @@
 {
   flake.nixosModules.waydroid = { pkgs, ... }: {
-    virtualisation.waydroid.enable = true;
+    virtualisation.waydroid = {
+      enable = true;
+      package = pkgs.waydroid-nftables;
+    };
+    networking.nftables.enable = true;
     environment.systemPackages = with pkgs; [
       wl-clipboard
     ];
