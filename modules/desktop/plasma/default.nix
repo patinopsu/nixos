8e5b3421819b97bafa6a49637f3649fc4d5c00fe
Patin Muangjan <patin@patin.dev>
2026-05-06 07:38:26 +0700

    feat: kde plasma~

---
+++ modules/desktop/plasma/default.nix
@@ -0,0 +1,25 @@
+{ inputs, self, ... }: {
+  flake.nixosModules.plasmade = { config, lib, pkgs, ... }: {
+    imports = [
+      self.nixosModules._1password
+    ];
+    services = {
+      desktopManager.plasma6.enable = true;
+      displayManager.plasma-login-manager.enable = true;;
+    };
+    home-manager.users.${config.vars.username} = {
+      imports = [
+        self.homeModules.browsers
+        self.homeModules.terminal
+        self.homeModules.vicinae
+        self.homeModules.spotify
+        self.homeModules.editors
+      ];
+    };
+    environment = {
+      systemPackages = with pkgs; [
+        klassy
+      ];
+    };
+  };
+}
