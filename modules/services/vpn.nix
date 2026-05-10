4757ab6d5321a0e52e9516fed1e297d1bf3a0698
Patin Muangjan <patin@patin.dev>
2026-05-10 22:57:45 +0700

    add tailscale0 to an umanaged interface

--- modules/services/vpn.nix
+++ modules/services/vpn.nix
@@ -1,10 +1,11 @@
 {
-  flake.nixosModules.vpn = { pkgs, ... }: {
+  flake.nixosModules.vpn = { config, pkgs, ... }: {
     services.tailscale.enable = true;
     networking.firewall = {
       trustedInterfaces = [ "tailscale0" ];
       allowedUDPPorts = [ config.services.tailscale.port ];
     };
+    networking.networkmanager.unmanaged = [ "tailscale0" ];
     systemd.services.tailscaled.serviceConfig.Environment = [
       "TS_DEBUG_FIREWALL_MODE=nftables"
     ];
