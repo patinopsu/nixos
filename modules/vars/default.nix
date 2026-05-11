580a4835a961a8b2d2e4f5e708efa7f9c0c4f8d6
Patin Muangjan <patin@patin.dev>
2026-05-11 23:36:56 +0700

    im cooking something hold on

--- modules/vars/default.nix
+++ modules/vars/default.nix
@@ -2,6 +2,14 @@
   flake = {
     nixosModules.base = { lib, ... }:  {
       options.vars = {
+        prettyname = lib.mkOption {
+          type = lib.types.str;
+          default = "undefined";
+        };
+        codename = lib.mkOption {
+          type = lib.types.str;
+          default = "undefined";
+        };
         configsrc = lib.mkOption {
           type = lib.types.path;
           default = self.outPath;
