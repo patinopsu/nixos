127fa036f5b06b8988fb0094c26f10d420c9eb72
Patin Muangjan <patin@patin.dev>
2026-05-11 23:18:03 +0700

    refactor vpn.nix

--- modules/services/vpn.nix
+++ modules/services/vpn.nix
@@ -1,11 +1,21 @@
 {
   flake.nixosModules.vpn = { config, pkgs, ... }: {
-    services.tailscale.enable = true;
-    networking.firewall = {
-      trustedInterfaces = [ "tailscale0" ];
-      allowedUDPPorts = [ config.services.tailscale.port ];
+    services = {
+      tailscale = {
+        enable = true;
+      };
     };
-    networking.networkmanager.unmanaged = [ "tailscale0" ];
+    networking = {
+      networkmanager = {
+        unmanaged = [
+          "tailscale0"
+        ];
+      };
+      firewall = {
+        trustedInterfaces = [ "tailscale0" ];
+        allowedUDPPorts = [ config.services.tailscale.port ];
+      };
+    };
     systemd.services.tailscaled.serviceConfig.Environment = [
       "TS_DEBUG_FIREWALL_MODE=nftables"
     ];
