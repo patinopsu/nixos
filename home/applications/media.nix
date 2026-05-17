675449654723c14025c1a29ed29d7e43d0f70405
Patin Muangjan <patin@patin.dev>
2026-05-17 12:52:26 +0700

    rename spotify.nix to media.nix
    cause it made more sense to group media player in to one!

---
+++ home/applications/media.nix
@@ -0,0 +1,32 @@
+{ inputs, self, ... }: {
+  flake.homeModules.media = { config, lib, pkgs, ... }: {
+    imports = [
+      inputs.spicetify-nix.homeManagerModules.default
+    ];
+    programs = {
+      mpv = {
+        enable = true;
+        config = {
+          keepaspect-window = "yes";
+          autofit = "50%x50%";
+          profile = "gpu-hq"; # High-quality rendering
+          hwdec = "auto-safe"; # Hardware acceleration
+          vo = "gpu";
+          ytdl-format = "bestvideo[height<=?1080]+bestaudio/best"; # Limit youtube-dl to 1080p
+        };
+      };
+      spicetify = let
+        spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.stdenv.hostPlatform.system};
+      in {
+        enable = true;
+        theme = spicePkgs.themes.text;
+        enabledExtensions = with spicePkgs.extensions; [
+          adblock
+          hidePodcasts
+          shuffle
+        ];
+      };
+    };
+    stylix.targets.spicetify.enable = false;
+  };
+}
