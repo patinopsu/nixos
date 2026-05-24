3f6630fd0e6ba77cab843f1d342a9a2d66a35c9f
Patin Muangjan <patin@patin.dev>
2026-05-24 18:12:23 +0700

    Tailscale stuff

--- modules/services/vpn.nix
+++ modules/services/vpn.nix
@@ -3,12 +3,20 @@
     services = {
       tailscale = {
         enable = true;
+        extraUpFlags = [
+          "--ssh"
+          "--accept-dns=false"
+          "--accept-routes=true"
+        ];
       };
     };
     networking = {
       networkmanager = {
         unmanaged = [
           "tailscale0"
+        ];
+        appendNameservers = [
+          "100.100.100.100"
         ];
       };
       firewall = {
@@ -18,8 +26,11 @@
         allowedUDPPorts = [ config.services.tailscale.port ];
       };
     };
-    systemd.services.tailscaled.serviceConfig.Environment = [
-      "TS_DEBUG_FIREWALL_MODE=nftables"
-    ];
+    systemd.services.tailscaled.serviceConfig = {
+      Environment = [
+        "TS_DEBUG_FIREWALL_MODE=nftables"
+      ];
+      RuntimeDirectoryMode = "0755";
+    };
   };
 }
